f69c67346267ce145ad2d51728cb57e6bc4da1a6
권장순 <jsoonworld@gmail.com>
2025-01-26 21:45:10 +0900

    feat(project) : setup project

---
+++ src/main/java/org/noostak/NoostakApplication.java
@@ -0,0 +1,13 @@
+package org.noostak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoostakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoostakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/noostak/NoostakApplicationTests.java
@@ -0,0 +1,13 @@
+package org.noostak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoostakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
