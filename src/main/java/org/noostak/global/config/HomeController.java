d7ea62bdbbd7a5bd072cc7c441aa6acde34b8abd
권장순 <jsoonworld@gmail.com>
2025-01-27 22:49:32 +0900

    refacotr(HomeController) : 경로 수정

--- src/main/java/org/noostak/global/config/HomeController.java
+++ src/main/java/org/noostak/global/config/HomeController.java
@@ -1,12 +1,12 @@
 package org.noostak.global.config;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 public class HomeController {
 
-    @GetMapping("/actuator/health")
+    @GetMapping("/")
     public String home() {
         return "Hello, World!";
     }
