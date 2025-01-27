2e0a9d68b8d36a7a7c703eca8d3e42e26c316df7
권장순 <jsoonworld@gmail.com>
2025-01-27 22:43:27 +0900

    refactor(HomeController) : 경로 수정

--- src/main/java/org/noostak/global/config/HomeController.java
+++ src/main/java/org/noostak/global/config/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/actuator/health")
     public String home() {
         return "Hello, World!";
     }
