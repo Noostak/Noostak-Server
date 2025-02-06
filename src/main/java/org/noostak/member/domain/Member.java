4d0bccd02fe17ffcd61f07600083bd5377249c85
권장순 <jsoonworld@gmail.com>
2025-02-06 19:22:11 +0900

    feat(Member) : 멤버 엔티티 구현
    - 멤버 ID, 이름, 프로필 이미지 키, 계정 상태, 인증 타입, 인증 ID, 리프레시 토큰 필드 추가
    - VO 및 Enum을 활용하여 멤버 속성 관리
    - `@AttributeOverride` 적용하여 필드명 명확화

---
+++ src/main/java/org/noostak/member/domain/Member.java
@@ -0,0 +1,38 @@
+package org.noostak.member.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.noostak.member.domain.vo.*;
+
+@Entity
+@Getter
+@RequiredArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Embedded
+    @AttributeOverride(name = "name", column = @Column(name = "member_name"))
+    private MemberName name;
+
+    @Embedded
+    @AttributeOverride(name = "key", column = @Column(name = "member_profile_image_key"))
+    private MemberProfileImageKey key;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "member_account_status")
+    private MemberAccountStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "auth_type")
+    private AuthType type;
+
+    @Embedded
+    @AttributeOverride(name = "id", column = @Column(name = "auth_id"))
+    private AuthId id;
+
+    private String refreshToken;
+}
