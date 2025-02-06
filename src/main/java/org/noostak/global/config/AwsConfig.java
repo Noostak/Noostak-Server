74d896893508e8584c8353d694c1fd8bd520abfc
0tae <choiyt3465@naver.com>
2025-02-06 20:43:27 +0900

    feat(AwsConfig) : AwsConfig 클래스 생성 및 기본 AWS 구성 추가 - S3 클라이언트 인스턴스 생성 구현부 작성 - AWS 접근 키(accessKey, secretKey)와 리전(awsRegion), 파일최대크기(maxFileSize)를 외부 프로퍼티로 부터 주입받는 구성

---
+++ src/main/java/org/noostak/global/config/AwsConfig.java
@@ -0,0 +1,52 @@
+package org.noostak.global.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+@Getter
+public class AwsConfig {
+
+    @Value("${aws-property.s3-bucket-name}")
+    private String s3BucketName;
+
+    @Value("${aws-property.access-key}")
+    private String accessKey;
+
+    @Value("${aws-property.secret-key}")
+    private String secretKey;
+
+    @Value("${aws-property.aws-region}")
+    private String awsRegion;
+
+    @Value("${aws-property.max-file-size}")
+    private String maxFileSize;
+
+    @Bean
+    public Region getRegion() {
+
+        return Region.of(awsRegion);
+    }
+
+    @Bean
+    public S3Client getS3Client() {
+        return S3Client.builder()
+                .region(getRegion())
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+
+    @Bean
+    public Long getMaxFileSize() {
+        try {
+            return Long.parseLong(maxFileSize);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid max file size format: " + maxFileSize, e);
+        }
+    }
+}
