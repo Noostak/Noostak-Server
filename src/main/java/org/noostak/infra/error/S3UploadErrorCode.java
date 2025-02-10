8aa16d9b884d77dc96c95c020989f919f0ee86ae
0tae <choiyt3465@naver.com>
2025-02-11 08:45:41 +0900

    refactor(S3UploadErrorCode): ErrorCode 구현부 추가

--- src/main/java/org/noostak/infra/error/S3UploadErrorCode.java
+++ src/main/java/org/noostak/infra/error/S3UploadErrorCode.java
@@ -3,11 +3,12 @@ package org.noostak.infra.error;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.noostak.global.error.core.ErrorCode;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
-public enum S3UploadErrorCode {
+public enum S3UploadErrorCode implements ErrorCode {
     INVALID_EXTENSION(HttpStatus.BAD_REQUEST, "이미지 확장자는 jpg, png, webp만 가능합니다."),
     FILE_SIZE_EXCEEDED(HttpStatus.BAD_REQUEST, "이미지 사이즈는 5MB를 넘을 수 없습니다.");
 
