f1214ab3d8ea33e23b0a2fa8482b82e9550e1fae
0tae <choiyt3465@naver.com>
2025-02-11 09:30:05 +0900

    Revert "feat(ErrorCode): ErrorCode Interface 정의"
    This reverts commit 2fec5e4eedaa765fa6c7036ccc1195203a21297f.

--- src/main/java/org/noostak/global/error/core/ErrorCode.java
+++
@@ -1,9 +0,0 @@
-package org.noostak.global.error.core;
-
-import org.springframework.http.HttpStatus;
-
-public interface ErrorCode {
-    HttpStatus getStatus();
-    String getMessage();
-    int getStatusValue();
-}
