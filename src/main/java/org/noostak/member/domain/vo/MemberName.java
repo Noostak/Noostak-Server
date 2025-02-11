94faa8331c0657e2f40f9d6ba8850ce6934b3a37
권장순 <jsoonworld@gmail.com>
2025-02-11 20:23:36 +0900

    refactor(MemberName) : 멤버 이름 유효성 검사 로직 개선 및 상수 적용
    - 최대 길이 검사를 위한 상수 MAX_LENGTH 적용
    - 숫자 포함 여부를 검사하는 validateNumbers 메서드 추가
    - 특수 문자 검사를 정규 표현식에서 개별 문자 검사 방식으로 변경
    - 한글 및 영문 외 문자 검사를 위한 isInvalidLanguage 메서드 추가
    - 불필요한 중복된 에러 코드 사용 정리

--- src/main/java/org/noostak/member/domain/vo/MemberName.java
+++ src/main/java/org/noostak/member/domain/vo/MemberName.java
@@ -8,6 +8,8 @@ import org.noostak.member.common.MemberException;
 @Embeddable
 @EqualsAndHashCode
 public class MemberName {
+    private static final int MAX_LENGTH = 15;
+
     private final String name;
 
     protected MemberName() {
@@ -30,7 +32,9 @@ public class MemberName {
     private void validateName(String name) {
         validateEmpty(name);
         validateLength(name);
+        validateNumbers(name);
         validateSpecialCharacters(name);
+        validateInvalidLanguages(name);
     }
 
     private void validateEmpty(String memberName) {
@@ -40,15 +44,43 @@ public class MemberName {
     }
 
     private void validateLength(String memberName) {
-        if (memberName.length() > 15) {
+        if (memberName.length() > MAX_LENGTH) {
             throw new MemberException(MemberErrorCode.MEMBER_NAME_LENGTH_EXCEEDED);
         }
     }
 
-    private void validateSpecialCharacters(String name) {
-        if (name.matches(".*[!@#$%^&*()_+=|<>?{}\\[\\]~-].*")) {
-            throw new MemberException(MemberErrorCode.MEMBER_NAME_INVALID_CHARACTER);
+    private void validateNumbers(String name) {
+        if (name.chars().anyMatch(Character::isDigit)) {
+            throw new MemberException(MemberErrorCode.MEMBER_NAME_CANNOT_CONTAIN_NUMBERS);
         }
+    }
+
+    private void validateSpecialCharacters(String name) {
+        if (name.chars().mapToObj(c -> (char) c).anyMatch(this::isSpecialCharacter)) {
+            throw new MemberException(MemberErrorCode.MEMBER_NAME_CANNOT_CONTAIN_SPECIAL_CHARACTERS);
+        }
+    }
+
+    private void validateInvalidLanguages(String name) {
+        if (name.chars().mapToObj(c -> (char) c).anyMatch(this::isInvalidLanguage)) {
+            throw new MemberException(MemberErrorCode.MEMBER_NAME_INVALID_LANGUAGE);
+        }
+    }
+
+    private boolean isKorean(char c) {
+        return (c >= '가' && c <= '힣');
+    }
+
+    private boolean isEnglish(char c) {
+        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+    }
+
+    private boolean isSpecialCharacter(char c) {
+        return !Character.isLetterOrDigit(c);
+    }
+
+    private boolean isInvalidLanguage(char c) {
+        return !isKorean(c) && !isEnglish(c);
     }
 
     @Override
