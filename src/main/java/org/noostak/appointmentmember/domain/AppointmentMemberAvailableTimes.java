ca79cb08df5996d41195a857998e2bdcb1f9a3bf
0tae <choiyt3465@naver.com>
2025-02-13 16:15:05 +0900

    feat(AppointmentMemberAvailableTimes) : 엔티티 클래스 구현

---
+++ src/main/java/org/noostak/appointmentmember/domain/AppointmentMemberAvailableTimes.java
@@ -0,0 +1,32 @@
+package org.noostak.appointmentmember.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.noostak.member.domain.Member;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@RequiredArgsConstructor
+public class AppointmentMemberAvailableTimes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long appointmentMemberAvailableTimeId;
+
+    @Column
+    private LocalDateTime appointmentMemberAvailableDate;
+
+    @Column
+    private LocalDateTime appointmentMemberAvailableStartTime;
+
+    @Column
+    private LocalDateTime appointmentMemberAvailableEndTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "appointment_member_id")
+    private AppointmentMember appointmentMember;
+
+}
