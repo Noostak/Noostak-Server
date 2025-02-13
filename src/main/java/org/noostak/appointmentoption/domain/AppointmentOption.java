b4b3f335993c24012c30027b55bd783e31af1b37
0tae <choiyt3465@naver.com>
2025-02-13 10:59:21 +0900

    refactor(AppointmentOptionTimeStatus) : VO 클래스 삭제

--- src/main/java/org/noostak/appointmentoption/domain/AppointmentOption.java
+++ src/main/java/org/noostak/appointmentoption/domain/AppointmentOption.java
@@ -4,9 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.noostak.appointmentoption.domain.vo.AppointmentOptionStatus;
-import org.noostak.appointmentoption.domain.vo.AppointmentOptionTimeStatus;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
@@ -22,12 +20,8 @@ public class AppointmentOption {
     @Column(name = "appointment_option_status")
     private AppointmentOptionStatus appointmentOptionStatus;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "appointment_option_time_status")
-    private AppointmentOptionTimeStatus appointmentOptionTimeStatus;
-
     @Column
-    private LocalDate appointmentOptionDate;
+    private LocalDateTime appointmentOptionDate;
 
     @Column
     private LocalDateTime appointmentOptionStartTime;
