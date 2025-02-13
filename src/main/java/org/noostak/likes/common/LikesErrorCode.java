a072fa8183924cc129a0e78d0c18151498e302fa
0tae <choiyt3465@naver.com>
2025-02-13 10:20:50 +0900

    refactor(LikeErrorCode) : 문자열 포맷팅 방식으로 message 정의 - OVER_MAX_LIKES 에서 최대 좋아요 수가 필요한 부분 %d 처리 - LikesCount.MAX_LIKES 사용 부분 제거

--- src/main/java/org/noostak/likes/common/LikesErrorCode.java
+++ src/main/java/org/noostak/likes/common/LikesErrorCode.java
@@ -3,14 +3,13 @@ package org.noostak.likes.common;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.noostak.global.error.core.ErrorCode;
-import org.noostak.likes.domain.vo.LikesCount;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum LikesErrorCode implements ErrorCode {
     LIKES_NOT_NEGATIVE(HttpStatus.BAD_REQUEST, "좋아요 수는 음수가 될 수 없습니다."),
-    OVER_MAX_LIKES(HttpStatus.BAD_REQUEST, "좋아요 수는 최대 " + LikesCount.MAX_LIKES + " 을/를 초과할 수 없습니다."),
+    OVER_MAX_LIKES(HttpStatus.BAD_REQUEST, "좋아요 수는 최대 %d을 초과할 수 없습니다."),
 
     ;
 
