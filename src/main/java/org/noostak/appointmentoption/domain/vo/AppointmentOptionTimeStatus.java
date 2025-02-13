d0f04ea04e6a0d3e898a5ddf5491ff466d9a89fc
0tae <choiyt3465@naver.com>
2025-02-13 09:23:40 +0900

    feat(AppointmentOptionStatus) : VO 클래스 구현 - AppointmentOptionStatus 및 AppointmentOptionTimeStatus 구현

---
+++ src/main/java/org/noostak/appointmentoption/domain/vo/AppointmentOptionTimeStatus.java
@@ -0,0 +1,17 @@
+package org.noostak.appointmentoption.domain.vo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum AppointmentOptionTimeStatus {
+
+    SELECTED("선택"),
+    UNSELECTED("미선택")
+
+    ;
+
+    private final String message;
+}
