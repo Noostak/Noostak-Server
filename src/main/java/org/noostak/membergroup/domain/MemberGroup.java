db685eed22025d682e4332c5b82294335fbe5e72
권장순 <jsoonworld@gmail.com>
2025-02-13 21:44:14 +0900

    feat(MemberGroup): 멤버 그룹 엔티티 구현

---
+++ src/main/java/org/noostak/membergroup/domain/MemberGroup.java
@@ -0,0 +1,35 @@
+package org.noostak.membergroup.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.noostak.global.entity.BaseTimeEntity;
+import org.noostak.group.domain.Group;
+import org.noostak.member.domain.Member;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class MemberGroup extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberGroupId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    private MemberGroup(final Member member, final Group group) {
+        this.member = member;
+        this.group = group;
+    }
+
+    public static MemberGroup of(final Member member, final Group group) {
+        return new MemberGroup(member, group);
+    }
+}
