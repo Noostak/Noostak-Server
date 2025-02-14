6c4fabff45111264adad8bff223b3015c7f4d599
권장순 <jsoonworld@gmail.com>
2025-02-14 22:51:02 +0900

    feat(GroupCreateInternalResponse) : 그룹 생성 내부 응답 DTO 추가
    - 그룹 생성 과정에서 내부적으로 사용되는 응답 DTO `GroupCreateInternalResponse` 추가
    - `Group` 엔터티와 `groupProfileImageUrl`(그룹 프로필 이미지 URL) 포함
    - 정적 팩토리 메서드 `of` 추가하여 객체 생성 편의성 제공

---
+++ src/main/java/org/noostak/group/dto/response/GroupCreateInternalResponse.java
@@ -0,0 +1,15 @@
+package org.noostak.group.dto.response;
+
+
+import org.noostak.group.domain.Group;
+
+public record GroupCreateInternalResponse(
+        Group group,
+        String groupProfileImageUrl
+) {
+    public static GroupCreateInternalResponse of(final Group group, final String groupProfileImageUrl) {
+        {
+            return new GroupCreateInternalResponse(group, groupProfileImageUrl);
+        }
+    }
+}
