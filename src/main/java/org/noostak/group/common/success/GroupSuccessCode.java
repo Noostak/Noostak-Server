cdafb317b535c1276363c590c8498ca0c9771d91
권장순 <jsoonworld@gmail.com>
2025-02-14 22:50:00 +0900

    feat(GroupSuccessCode) : 그룹 관련 성공 응답 코드 추가
    - `GROUP_CREATED` : 그룹 생성 성공 시 반환되는 응답 코드 (`HttpStatus.CREATED`)
    - 성공 응답 메시지로 `"그룹이 성공적으로 생성되었습니다."` 추가
    - `SuccessCode` 인터페이스를 구현하여 일관된 성공 응답 코드 관리

---
+++ src/main/java/org/noostak/group/common/success/GroupSuccessCode.java
@@ -0,0 +1,16 @@
+package org.noostak.group.common.success;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.noostak.global.success.SuccessCode;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum GroupSuccessCode implements SuccessCode {
+    GROUP_CREATED(HttpStatus.CREATED, "그룹이 성공적으로 생성되었습니다."),
+    ;
+
+    private final HttpStatus status;
+    private final String message;
+}
