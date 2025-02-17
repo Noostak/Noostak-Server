4b325d574b3c59e85cb4bd4a52bcded67e1c9e6d
권장순 <jsoonworld@gmail.com>
2025-02-17 19:58:11 +0900

    feat(GroupInfoServiceImpl) : 그룹 정보 조회 서비스 구현
    - `GroupInfoService` 구현체 `GroupInfoServiceImpl` 추가
    - 그룹 정보를 조회하고, 그룹장 및 그룹원 정보를 포함하는 `GroupInfoResponse` 반환
    - 그룹 및 멤버 정보 조회 시 존재하지 않을 경우 예외 처리 (`GroupException` 활용)
    - `S3Service`를 이용하여 그룹 및 멤버 프로필 이미지 URL 조회

---
+++ src/main/java/org/noostak/group/application/GroupInfoServiceImpl.java
@@ -0,0 +1,91 @@
+package org.noostak.group.application;
+
+import lombok.RequiredArgsConstructor;
+import org.noostak.group.common.exception.GroupErrorCode;
+import org.noostak.group.common.exception.GroupException;
+import org.noostak.group.domain.Group;
+import org.noostak.group.domain.GroupRepository;
+import org.noostak.group.dto.response.info.GroupInfoResponse;
+import org.noostak.group.dto.response.info.GroupMemberInfoResponse;
+import org.noostak.group.dto.response.info.GroupSummaryResponse;
+import org.noostak.infra.S3Service;
+import org.noostak.member.domain.Member;
+import org.noostak.membergroup.domain.MemberGroupRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GroupInfoServiceImpl implements GroupInfoService {
+
+    private final GroupRepository groupRepository;
+    private final MemberGroupRepository memberGroupRepository;
+    private final S3Service s3Service;
+
+    @Override
+    public GroupInfoResponse getGroupInfo(final Long memberId, final Long groupId) {
+        Group group = findGroupById(groupId);
+
+        Member member = findMemberInGroup(memberId, groupId);
+        GroupMemberInfoResponse myInfoResponse = convertToGroupMemberInfo(member);
+
+
+        GroupMemberInfoResponse groupHostInfo = findGroupHost(groupId);
+
+        List<GroupMemberInfoResponse> groupMembersInfo = findGroupMembers(groupId);
+
+        GroupSummaryResponse groupSummaryResponse = convertToGroupSummaryResponse(group, groupHostInfo, groupMembersInfo);
+
+        return GroupInfoResponse.of(myInfoResponse, groupSummaryResponse);
+    }
+
+    private Member findMemberInGroup(Long memberId, Long groupId) {
+        return memberGroupRepository.findMembersByGroupId(groupId).stream()
+                .filter(m -> m.getMemberId().equals(memberId))
+                .findFirst()
+                .orElseThrow(() -> new GroupException(GroupErrorCode.MEMBER_NOT_FOUND));
+    }
+
+    private Group findGroupById(Long groupId) {
+        return groupRepository.findById(groupId)
+                .orElseThrow(() -> new GroupException(GroupErrorCode.GROUP_NOT_FOUND));
+    }
+
+    private GroupMemberInfoResponse findGroupHost(Long groupId) {
+        Member host = memberGroupRepository.findGroupHostByGroupId(groupId);
+        if (host == null) throw new GroupException(GroupErrorCode.GROUP_MEMBER_NOT_FOUND);
+
+        return convertToGroupMemberInfo(host);
+    }
+
+    private List<GroupMemberInfoResponse> findGroupMembers(Long groupId) {
+        return memberGroupRepository.findMembersByGroupId(groupId).stream()
+                .map(this::convertToGroupMemberInfo)
+                .toList();
+    }
+
+    private GroupMemberInfoResponse convertToGroupMemberInfo(Member member) {
+        return GroupMemberInfoResponse.of(
+                member.getName().value(),
+                s3Service.getImageUrl(member.getKey().value())
+        );
+    }
+
+    private GroupSummaryResponse convertToGroupSummaryResponse(Group group, GroupMemberInfoResponse hostInfo, List<GroupMemberInfoResponse> membersInfo) {
+        String groupProfileImageUrl = getGroupProfileImageUrl(group);
+
+        return GroupSummaryResponse.of(
+                hostInfo,
+                group.getName().value(),
+                groupProfileImageUrl,
+                group.getCount().value(),
+                group.getCode().value(),
+                membersInfo
+        );
+    }
+
+    private String getGroupProfileImageUrl(Group group) {
+        return s3Service.getImageUrl(group.getKey().value());
+    }
+}
