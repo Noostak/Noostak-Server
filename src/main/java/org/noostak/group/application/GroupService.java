32c36f1251f2739144f34bdebe4e3c6efcc858b1
권장순 <jsoonworld@gmail.com>
2025-02-17 19:58:47 +0900

    feat(GroupService) : 그룹 정보 조회 기능 추가
    - `GroupInfoService`를 활용한 `getGroupInfo(Long memberId, Long groupId)` 메서드 추가
    - DTO 패키지 변경 사항 반영 (`GroupCreateInternalResponse`, `GroupCreateResponse`, `GroupsRetrieveResponse`)
    - `groupInfoService`를 의존성으로 추가하여 그룹 정보 조회 기능 제공

--- src/main/java/org/noostak/group/application/GroupService.java
+++ src/main/java/org/noostak/group/application/GroupService.java
@@ -2,9 +2,10 @@ package org.noostak.group.application;
 
 import lombok.RequiredArgsConstructor;
 import org.noostak.group.dto.request.GroupCreateRequest;
-import org.noostak.group.dto.response.GroupCreateInternalResponse;
-import org.noostak.group.dto.response.GroupCreateResponse;
-import org.noostak.group.dto.response.GroupsRetrieveResponse;
+import org.noostak.group.dto.response.create.GroupCreateInternalResponse;
+import org.noostak.group.dto.response.create.GroupCreateResponse;
+import org.noostak.group.dto.response.info.GroupInfoResponse;
+import org.noostak.group.dto.response.retrieve.GroupsRetrieveResponse;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -15,6 +16,7 @@ public class GroupService {
 
     private final GroupCreateService groupCreateService;
     private final GroupRetrieveService groupRetrieveService;
+    private final GroupInfoService groupInfoService;
 
     public GroupCreateResponse createGroup(Long memberId, GroupCreateRequest request) throws IOException {
         GroupCreateInternalResponse response = groupCreateService.createGroup(memberId, request);
@@ -23,5 +25,9 @@ public class GroupService {
 
     public GroupsRetrieveResponse findGroups(Long memberId) {
         return groupRetrieveService.findGroups(memberId);
+    }
+
+    public GroupInfoResponse getGroupInfo(Long memberId, Long groupId) {
+        return groupInfoService.getGroupInfo(memberId, groupId);
     }
 }
