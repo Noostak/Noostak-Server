8631530ec0a949813810491076978f6ec698ac0c
권장순 <jsoonworld@gmail.com>
2025-02-18 18:27:38 +0900

    feat(AppointmentSuccessCode) : 약속 시간 저장 성공 코드 추가
    약속 가능 시간을 저장할 때 성공 메시지를 반환할 수 있도록 SUCCESS_SAVE_AVAILABLE_TIMES 코드를 추가했습니다.

--- src/main/java/org/noostak/appointment/common/success/AppointmentSuccessCode.java
+++ src/main/java/org/noostak/appointment/common/success/AppointmentSuccessCode.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum AppointmentSuccessCode implements SuccessCode {
     APPOINTMENT_CREATED(HttpStatus.CREATED, "약속이 성공적으로 생성되었습니다."),
+
+    SUCCESS_SAVE_AVAILABLE_TIMES(HttpStatus.OK, "약속 시간이 성공적으로 저장되었습니다."),
     ;
 
     private final HttpStatus status;
