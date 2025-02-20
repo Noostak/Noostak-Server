6f458a018af8097e990d8878aece64023f496bcb
권장순 <jsoonworld@gmail.com>
2025-02-20 17:56:06 +0900

    refactor(GroupRepositoryTest) : 패키지 구조 정리 및 불필요한 중복 제거
    - `GroupRepositoryTest`의 패키지를 `org.noostak.server.group.domain`에서 `org.noostak.group.domain`으로 이동
    - 중복된 `import org.noostak.server.group.domain.GroupRepositoryTest;` 제거
    - 관련된 테스트 코드에서 변경된 패키지를 참조하도록 수정하여 일관성 유지

--- src/test/java/org/noostak/group/application/GroupRetrieveServiceImplTest.java
+++ src/test/java/org/noostak/group/application/GroupRetrieveServiceImplTest.java
@@ -8,6 +8,7 @@ import org.noostak.group.common.exception.GroupErrorCode;
 import org.noostak.group.common.exception.GroupException;
 import org.noostak.group.domain.Group;
 import org.noostak.group.domain.GroupRepository;
+import org.noostak.group.domain.GroupRepositoryTest;
 import org.noostak.group.domain.vo.GroupName;
 import org.noostak.group.domain.vo.GroupProfileImageKey;
 import org.noostak.group.dto.response.retrieve.GroupsRetrieveResponse;
@@ -22,7 +23,6 @@ import org.noostak.member.domain.vo.MemberProfileImageKey;
 import org.noostak.membergroup.MemberGroupRepositoryTest;
 import org.noostak.membergroup.domain.MemberGroup;
 import org.noostak.membergroup.domain.MemberGroupRepository;
-import org.noostak.server.group.domain.GroupRepositoryTest;
 
 import java.util.List;
 
