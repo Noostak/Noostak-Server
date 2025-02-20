a111cd0e5b222bf638e4b685d6fc30213c1d6ad5
0tae <choiyt3465@naver.com>
2025-02-20 18:54:25 +0900

    fix(AuthInfoRepository) : AuthId VO 도입으로 인한 코드 수정

--- src/main/java/org/noostak/auth/domain/AuthInfoRepository.java
+++ src/main/java/org/noostak/auth/domain/AuthInfoRepository.java
@@ -2,7 +2,7 @@ package org.noostak.auth.domain;
 
 import org.noostak.auth.common.exception.AuthErrorCode;
 import org.noostak.auth.common.exception.AuthException;
-import org.noostak.auth.domain.vo.Code;
+import org.noostak.auth.domain.vo.AuthId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,15 +11,15 @@ import java.util.Optional;
 @Repository
 public interface AuthInfoRepository extends JpaRepository<AuthInfo, Long> {
 
-    Optional<AuthInfo> findByAuthId(Code code);
-    boolean existsAuthInfoByAuthId(Code code);
+    Optional<AuthInfo> findByAuthId(AuthId authid);
+    boolean existsAuthInfoByAuthId(AuthId code);
 
-    default boolean hasAuthInfoByAuthId(Code code){
-        return this.existsAuthInfoByAuthId(code);
+    default boolean hasAuthInfoByAuthId(AuthId authid){
+        return this.existsAuthInfoByAuthId(authid);
     }
 
-    default AuthInfo getAuthInfoByAuthId(Code code){
-        return this.findByAuthId(code)
-                .orElseThrow(()->new AuthException(AuthErrorCode.AUTH_INFO_NOT_EXISTS, code));
+    default AuthInfo getAuthInfoByAuthId(AuthId authid){
+        return this.findByAuthId(authid)
+                .orElseThrow(()->new AuthException(AuthErrorCode.AUTH_INFO_NOT_EXISTS, authid));
     }
 }
