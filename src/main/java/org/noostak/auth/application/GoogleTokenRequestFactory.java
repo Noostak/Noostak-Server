395b05197f644ede45fa29922a19947435b500dd
0tae <choiyt3465@naver.com>
2025-02-21 06:40:20 +0900

    feat(GoogleApiException) : 구글 토큰 요청 request DTO 생성 팩토리 구현

---
+++ src/main/java/org/noostak/auth/application/GoogleTokenRequestFactory.java
@@ -0,0 +1,29 @@
+package org.noostak.auth.application;
+
+import org.noostak.auth.dto.GoogleTokenRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class GoogleTokenRequestFactory {
+
+    @Value("${oauth-property.google.client_id}")
+    private final String clientId;
+
+    @Value("${oauth-property.google.redirect_uri}")
+    private final String redirectUri;
+
+    @Value("${oauth-property.google.client_secret}")
+    private final String clientSecret;
+
+    public GoogleTokenRequestFactory(String clientId, String redirectUri, String clientSecret) {
+        this.clientId = clientId;
+        this.redirectUri = redirectUri;
+        this.clientSecret = clientSecret;
+    }
+
+    public GoogleTokenRequest createRequest(String code) {
+        return GoogleTokenRequest.of(clientId, redirectUri, code, clientSecret);
+    }
+}
