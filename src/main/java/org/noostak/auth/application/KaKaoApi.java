2fb5499439181a2d02941e18d9436368772db339
0tae <choiyt3465@naver.com>
2025-02-20 19:17:05 +0900

    fix(KaKaoApi) : enum 클래스 수정 및 추가 - FETCH_USER_INFO_REQUEST -> FETCH_TOKEN 명칭 변경 - 액세스 토큰으로 내 정보를 가져오는 FETCH_MY_INFO 추가

--- src/main/java/org/noostak/auth/application/KaKaoApi.java
+++ src/main/java/org/noostak/auth/application/KaKaoApi.java
@@ -8,8 +8,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum KaKaoApi {
     TOKEN_REQUEST("https://kauth.kakao.com/oauth/token"),
-    FETCH_USER_INFO_REQUEST("https://kapi.kakao.com/v2/user/me")
-
+    FETCH_TOKEN("https://kapi.kakao.com/v1/user/access_token_info"),
+    FETCH_MY_INFO("https://kapi.kakao.com/v2/user/me")
     ;
 
     private final String url;
