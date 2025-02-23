e092bcce69596a6fc7a48da0d704dff3cb558dfe
0tae <choiyt3465@naver.com>
2025-02-23 17:15:54 +0900

    feat(AuthSuccessCode) : TOKEN_REISSUE_COMPLETED 상수 추가

--- src/main/java/org/noostak/auth/common/success/AuthSuccessCode.java
+++ src/main/java/org/noostak/auth/common/success/AuthSuccessCode.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum AuthSuccessCode implements SuccessCode {
     SIGN_UP_COMPLETED(HttpStatus.CREATED, "회원가입이 성공적으로 완료 되었습니다."),
     SIGN_IN_COMPLETED(HttpStatus.OK, "소셜 로그인에 성공했습니다."),
+    TOKEN_REISSUE_COMPLETED(HttpStatus.OK, "토큰 재발급에 성공하였습니다.")
     ;
 
     private final HttpStatus status;
