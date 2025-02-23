05f602eb230c2598465a89d915d90d8b3644d836
0tae <choiyt3465@naver.com>
2025-02-23 17:15:02 +0900

    refactor(KaKaoApi) : FETCH_TOKEN 상수 삭제

--- src/main/java/org/noostak/auth/application/KaKaoApi.java
+++ src/main/java/org/noostak/auth/application/KaKaoApi.java
@@ -8,7 +8,6 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum KaKaoApi {
     TOKEN_REQUEST("https://kauth.kakao.com/oauth/token"),
-    FETCH_TOKEN("https://kapi.kakao.com/v1/user/access_token_info"),
     USER_INFO("https://kapi.kakao.com/v2/user/me")
     ;
 
