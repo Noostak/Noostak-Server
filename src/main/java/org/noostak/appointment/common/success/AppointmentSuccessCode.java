83954ecacd6582b736fc90212aa9a686901f44df
권장순 <jsoonworld@gmail.com>
2025-02-24 13:52:41 +0900

    feat(AppointmentSuccessCode) : 약속 확정 성공 메시지 추가
    - AppointmentSuccessCode에 APPOINTMENT_CONFIRMED 코드 추가
    - 약속 확정 시 성공 응답으로 사용될 메시지 정의

--- src/main/java/org/noostak/appointment/common/success/AppointmentSuccessCode.java
+++ src/main/java/org/noostak/appointment/common/success/AppointmentSuccessCode.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum AppointmentSuccessCode implements SuccessCode {
     APPOINTMENT_CREATED(HttpStatus.CREATED, "약속이 성공적으로 생성되었습니다."),
+
+    APPOINTMENT_CONFIRMED(HttpStatus.OK, "약속이 성공적으로 확정되었습니다."),
     ;
 
     private final HttpStatus status;
