2b07a0e75f0dac782e9995a585d08467979cc3af
권장순 <jsoonworld@gmail.com>
2025-02-25 08:58:38 +0900

    feat(AppointmentMemberRepositoryCustom) : 약속 선택 여부

--- src/main/java/org/noostak/appointmentmember/domain/AppointmentMemberRepositoryCustom.java
+++ src/main/java/org/noostak/appointmentmember/domain/AppointmentMemberRepositoryCustom.java
@@ -6,4 +6,6 @@ import java.util.Optional;
 public interface AppointmentMemberRepositoryCustom {
     Optional<AppointmentMember> findByMemberIdAndAppointmentId(Long memberId, Long appointmentId);
     List<AppointmentMember> findAllWithAvailableTimes(Long appointmentId);
+    Long countMembersWithTimeSet(Long appointmentId);
 }
+
