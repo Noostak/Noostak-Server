fc4ab6c33393400bed8f67e257b04da827f6e445
0tae <choiyt3465@naver.com>
2025-02-26 00:21:16 +0900

    feat(AppointmentRepository) : getById default 메소드 추가 - 해당 Repository 내부에서 예외처리 진행

--- src/main/java/org/noostak/appointment/domain/AppointmentRepository.java
+++ src/main/java/org/noostak/appointment/domain/AppointmentRepository.java
@@ -1,8 +1,15 @@
 package org.noostak.appointment.domain;
 
+import org.noostak.appointment.common.exception.AppointmentErrorCode;
+import org.noostak.appointment.common.exception.AppointmentException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    default Appointment getById(Long appointmentId){
+        return findById(appointmentId)
+                .orElseThrow(()-> new AppointmentException(AppointmentErrorCode.APPOINTMENT_NOT_FOUND));
+    }
+
 }
