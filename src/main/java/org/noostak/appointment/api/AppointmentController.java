d565245919fe1493da614473a9ba9337f2913ac1
권장순 <jsoonworld@gmail.com>
2025-02-25 13:18:11 +0900

    refactor(AppointmentController) : 중복된 @RequestMapping 제거
    - `@RequestMapping("/api/v1/groups/{groupId}/appointments")`와 `@RequestMapping("/api/v1/appointments")` 중복 제거
    - API 경로의 일관성을 유지하도록 수정

--- src/main/java/org/noostak/appointment/api/AppointmentController.java
+++ src/main/java/org/noostak/appointment/api/AppointmentController.java
@@ -2,28 +2,12 @@ package org.noostak.appointment.api;
 
 import lombok.RequiredArgsConstructor;
 import org.noostak.appointment.application.AppointmentService;
-import org.noostak.appointment.dto.request.AppointmentCreateRequest;
-import org.noostak.global.success.SuccessResponse;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static org.noostak.appointment.common.success.AppointmentSuccessCode.APPOINTMENT_CREATED;
 
 @RestController
-@RequestMapping("/api/v1/groups/{groupId}/appointments")
+@RequestMapping("/api/v1/appointments")
 @RequiredArgsConstructor
 public class AppointmentController {
 
-    private final AppointmentService appointmentService;
-
-    @PostMapping
-    public ResponseEntity<SuccessResponse> createAppointment(
-            // @AuthenticationPrincipal Long memberId,
-            @PathVariable(name = "groupId") Long groupId,
-            @RequestBody AppointmentCreateRequest request
-    ) {
-        Long memberId = 1L;
-        appointmentService.createAppointment(memberId, groupId, request);
-        return ResponseEntity.ok(SuccessResponse.of(APPOINTMENT_CREATED));
-    }
 }
