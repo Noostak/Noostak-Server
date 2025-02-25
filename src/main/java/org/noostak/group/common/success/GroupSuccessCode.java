e9141ef7eae22e352b569cdfdc6b2b6d9342ff63
권장순 <jsoonworld@gmail.com>
2025-02-25 13:24:30 +0900

    fix(GroupSuccessCode) : 중복된 GROUP_INFO 성공 코드 제거
    - `GROUP_INFO` 성공 코드 중복 제거
    - 그룹 관련 성공 메시지 수정

--- src/main/java/org/noostak/group/common/success/GroupSuccessCode.java
+++ src/main/java/org/noostak/group/common/success/GroupSuccessCode.java
@@ -12,7 +12,9 @@ public enum GroupSuccessCode implements SuccessCode {
 
     GROUP_RETRIEVED(HttpStatus.OK, "그룹이 성공적으로 조회되었습니다."),
 
-    GROUP_INFO(HttpStatus.OK, "그룹 정보가 성공적으로 조회되었습니다.")
+    GROUP_INFO(HttpStatus.OK, "그룹 정보가 성공적으로 조회되었습니다."),
+
+    APPOINTMENT_CREATED(HttpStatus.CREATED, "약속이 성공적으로 생성되었습니다."),
     ;
 
     private final HttpStatus status;
