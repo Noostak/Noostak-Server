4576d5f2a4bac4cb71d81b94d9737b429f366c34
0tae <choiyt3465@naver.com>
2025-02-26 00:22:50 +0900

    feat(LikeRepository) : LikeRepository 인터페이스 정의 - optionId에 해당된 Like 갯수를 구하는 getLikeCountByOptionId 구현 - appointmentMemberId에 해당된 Like 갯수를 구하는 getLikeCountByAppointmentMemberId 구현

---
+++ src/main/java/org/noostak/likes/domain/LikeRepository.java
@@ -0,0 +1,19 @@
+package org.noostak.likes.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT COUNT(*) FROM likes WHERE appointment_option_id = :optionId")
+    int getLikeCountByOptionId(@Param("optionId") Long optionId);
+
+    @Query(nativeQuery = true, value = "SELECT COUNT(*) FROM likes WHERE appointment_member_id = :appointmentMemberId")
+    int getLikeCountByAppointmentMemberId(@Param("appointmentMemberId") Long appointmentMemberId);
+
+}
+
