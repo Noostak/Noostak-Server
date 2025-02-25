e2d42d7223026e7dec5ef889d726ff573b77896b
권장순 <jsoonworld@gmail.com>
2025-02-25 13:19:34 +0900

    refactor(AppointmentService) : 패키지 변경에 따른 의존성 수정
    - `AppointmentCreateService`의 패키지가 변경되어, `org.noostak.group.application.create`로 수정
    - `AppointmentCreateRequest` DTO의 패키지 변경에 따라 import 경로 수정

--- src/main/java/org/noostak/appointment/application/AppointmentService.java
+++ src/main/java/org/noostak/appointment/application/AppointmentService.java
@@ -1,17 +1,13 @@
 package org.noostak.appointment.application;
 
 import lombok.RequiredArgsConstructor;
-import org.noostak.appointment.dto.request.AppointmentCreateRequest;
+import org.noostak.group.application.create.AppointmentCreateService;
+import org.noostak.group.dto.request.AppointmentCreateRequest;
 import org.noostak.appointmentoption.application.AppointmentOptionConfirmService;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AppointmentService {
-    private final AppointmentCreateService appointmentCreateService;
-    private final AppointmentOptionConfirmService appointmentConfirmService;
 
-    public void createAppointment(Long memberId, Long groupId, AppointmentCreateRequest request) {
-        appointmentCreateService.createAppointment(memberId, groupId, request);
-    }
 }
