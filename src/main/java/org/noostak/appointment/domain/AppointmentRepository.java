9bb601f0a81d43bef57f24cee951ebb0c9a82148
0tae <choiyt3465@naver.com>
2025-02-26 20:06:01 +0900

    feat(AppointmentRepository) : 약속 필터링 조회 쿼리 로직 작성

--- src/main/java/org/noostak/appointment/domain/AppointmentRepository.java
+++ src/main/java/org/noostak/appointment/domain/AppointmentRepository.java
@@ -1,8 +1,25 @@
 package org.noostak.appointment.domain;
 
+import org.noostak.appointment.domain.vo.AppointmentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    @Query(nativeQuery = true, value =
+            "SELECT * " +
+                    "FROM appointment " +
+                    "WHERE appointment_status = :status "+
+                    "AND groups_id = :groupId")
+    List<Appointment> findAllByGroupIdConfirmed(String status, Long groupId);
+
+
+    default List<Appointment> getAllByGroupIdConfirmed(AppointmentStatus status, Long groupId){
+        return findAllByGroupIdConfirmed(status.name(),groupId);
+    }
+
 }
