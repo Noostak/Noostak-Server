1b6801e64cade0eaa4b89e8020387c2c10fd8e2d
권장순 <jsoonworld@gmail.com>
2025-02-27 20:17:40 +0900

    feat(AppointmentRepositoryImpl) : 그룹별 확정된 예약 조회 기능 구현
    - `findAllByGroupIdConfirmed` 메서드 추가하여 특정 그룹 내 확정된 예약만 조회 가능하도록 구현
    - QueryDSL을 활용하여 `appointmentStatus`와 `groupId` 조건으로 필터링하여 예약 목록 반환

--- src/main/java/org/noostak/appointment/domain/AppointmentRepositoryImpl.java
+++ src/main/java/org/noostak/appointment/domain/AppointmentRepositoryImpl.java
@@ -2,9 +2,9 @@ package org.noostak.appointment.domain;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.noostak.appointment.domain.vo.AppointmentStatus;
 
 import java.util.List;
-
 
 import static org.noostak.appointment.domain.QAppointment.appointment;
 
@@ -19,6 +19,17 @@ public class AppointmentRepositoryImpl implements AppointmentRepositoryCustom {
                 .selectFrom(appointment)
                 .where(appointment.group.id.eq(groupId))
                 .orderBy(appointment.createdAt.desc())
+                .fetch();
+    }
+
+    @Override
+    public List<Appointment> findAllByGroupIdConfirmed(AppointmentStatus status, Long groupId) {
+        return queryFactory
+                .selectFrom(appointment)
+                .where(
+                        appointment.appointmentStatus.eq(status),
+                        appointment.group.id.eq(groupId)
+                )
                 .fetch();
     }
 }
