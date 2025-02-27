48e539490ae411272a1a5ec8666a39753f7b829c
권장순 <jsoonworld@gmail.com>
2025-02-27 17:18:29 +0900

    refactor(AppointmentMemberRepositoryCustom) : 메서드 정리 및 가독성 개선
    - `findByAppointmentId` 메서드 유지하며 인터페이스 코드 정리
    - 가독성 향상을 위해 공백 조정 및 일관된 코드 스타일 적용

--- src/main/java/org/noostak/appointmentmember/domain/AppointmentMemberRepositoryCustom.java
+++ src/main/java/org/noostak/appointmentmember/domain/AppointmentMemberRepositoryCustom.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 public interface AppointmentMemberRepositoryCustom {
     Optional<AppointmentMember> findByMemberIdAndAppointmentId(Long memberId, Long appointmentId);
     List<AppointmentMember> findAllWithAvailableTimes(Long appointmentId);
+    List<AppointmentMember> findByAppointmentId(Long appointmentId);
 }
