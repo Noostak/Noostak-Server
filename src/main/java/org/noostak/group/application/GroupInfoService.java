2a54384b610c966ec883cfd94835cf1b1f5ef51e
권장순 <jsoonworld@gmail.com>
2025-02-28 00:04:49 +0900

    refactor(service) : 패키지 이동

--- src/main/java/org/noostak/group/application/GroupInfoService.java
+++
@@ -1,7 +0,0 @@
-package org.noostak.group.application;
-
-import org.noostak.group.dto.response.info.GroupInfoResponse;
-
-public interface GroupInfoService {
-    GroupInfoResponse getGroupInfo(Long memberId, Long groupId);
-}
--- src/main/java/org/noostak/group/application/GroupRetrieveService.java
+++
@@ -1,7 +0,0 @@
-package org.noostak.group.application;
-
-import org.noostak.group.dto.response.retrieve.GroupsRetrieveResponse;
-
-public interface GroupRetrieveService {
-    GroupsRetrieveResponse findGroups(Long memberId);
-}
