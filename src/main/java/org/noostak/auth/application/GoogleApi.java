03595a6ff1c0ff4c1274c7a5cfeb2dea3b9004d7
0tae <choiyt3465@naver.com>
2025-02-27 13:47:42 +0900

    feat(GoogleApi) : GoogleApi URL 상수 클래스 정의 - Logout, Unlink URL 정의

--- src/main/java/org/noostak/auth/application/GoogleApi.java
+++ src/main/java/org/noostak/auth/application/GoogleApi.java
@@ -8,7 +8,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum GoogleApi {
     TOKEN_REQUEST("https://oauth2.googleapis.com/token"),
-    USER_INFO("https://www.googleapis.com/oauth2/v2/userinfo")
+    USER_INFO("https://www.googleapis.com/oauth2/v2/userinfo"),
+    LOGOUT("https://kauth.kakao.com/oauth/logout"),
+    UNLINK("https://oauth2.googleapis.com/revoke")
+
     ;
 
     private final String url;
