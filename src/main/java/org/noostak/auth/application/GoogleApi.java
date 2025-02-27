bc6101aea4faa1d3674fc93e78a96e750d13ed69
0tae <choiyt3465@naver.com>
2025-02-27 15:26:49 +0900

    fix(GoogleApi) : GoogleApi URL 상수 클래스 삭제 - Logout 링크 삭제

--- src/main/java/org/noostak/auth/application/GoogleApi.java
+++ src/main/java/org/noostak/auth/application/GoogleApi.java
@@ -9,7 +9,6 @@ import lombok.Getter;
 public enum GoogleApi {
     TOKEN_REQUEST("https://oauth2.googleapis.com/token"),
     USER_INFO("https://www.googleapis.com/oauth2/v2/userinfo"),
-    LOGOUT("https://kauth.kakao.com/oauth/logout"),
     UNLINK("https://oauth2.googleapis.com/revoke")
 
     ;
