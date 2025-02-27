7b8889d06e45cf90db386d6b0428e14fec5b99a6
권장순 <jsoonworld@gmail.com>
2025-02-27 20:17:19 +0900

    feat(AppointmentRepositoryCustom) : 그룹별 확정된 예약 조회 기능 추가
    - `findAllByGroupIdConfirmed` 메서드 추가하여 특정 그룹 내 확정된 예약만 조회 가능하도록 구현

--- src/main/java/org/noostak/appointment/domain/AppointmentRepositoryCustom.java
+++ src/main/java/org/noostak/appointment/domain/AppointmentRepositoryCustom.java
@@ -1,7 +1,10 @@
 package org.noostak.appointment.domain;
 
+import org.noostak.appointment.domain.vo.AppointmentStatus;
+
 import java.util.List;
 
 public interface AppointmentRepositoryCustom {
     List<Appointment> findAllByGroupId(Long groupId);
+    List<Appointment> findAllByGroupIdConfirmed(AppointmentStatus status, Long groupId);
 }
