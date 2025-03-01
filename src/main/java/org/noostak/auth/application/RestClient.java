41f4b9c2a78f400106cb2202bb080f7bbc5d5e9a
0tae <choiyt3465@naver.com>
2025-03-01 17:45:45 +0900

    fix(RestTemplate) : 로그 추가

--- src/main/java/org/noostak/auth/application/RestClient.java
+++ src/main/java/org/noostak/auth/application/RestClient.java
@@ -2,6 +2,7 @@ package org.noostak.auth.application;
 
 import org.noostak.auth.common.exception.RestClientErrorCode;
 import org.noostak.auth.common.exception.RestClientException;
+import org.noostak.global.utils.GlobalLogger;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -22,6 +23,7 @@ public class RestClient {
      * 기본적인 JSON 요청
      */
     public <T, R> T postRequest(String url, R request, Class<T> responseType) {
+        GlobalLogger.info("request : ",url,request);
         try {
             T response = restTemplate.postForObject(url, request, responseType);
             return validate(response);
@@ -34,6 +36,8 @@ public class RestClient {
      * x-www-form-urlencoded 요청
      */
     public <T> T postRequest(String url, String urlParams, Class<T> responseType) {
+        GlobalLogger.info("request : ", url, urlParams);
+
         try {
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -51,6 +55,7 @@ public class RestClient {
      * 커스텀 헤더와 함께 JSON 요청
      */
     public <T, R> T postRequest(String url, HttpHeaders headers, R request, Class<T> responseType) {
+        GlobalLogger.info("request : ",url,headers,request);
         try {
             HttpEntity<R> requestHttpEntity = new HttpEntity<>(request, headers);
             T response = restTemplate.postForObject(url, requestHttpEntity, responseType);
@@ -64,6 +69,8 @@ public class RestClient {
      * 커스텀 헤더만 포함된 요청
      */
     public <T> T postRequest(String url, HttpHeaders headers, Class<T> responseType) {
+        GlobalLogger.info("request : ",url,headers);
+
         try {
             HttpEntity<String> entity = new HttpEntity<>(headers);
             T response = restTemplate.postForObject(url, entity, responseType);
@@ -75,6 +82,8 @@ public class RestClient {
     }
 
     public <T> T getRequest(String url, HttpHeaders headers, Class<T> responseType) {
+        GlobalLogger.info("request : ",url,headers);
+
         try {
             HttpEntity<String> entity = new HttpEntity<>(headers);
             T response = restTemplate.exchange(url, HttpMethod.GET,entity, responseType).getBody();
@@ -85,6 +94,8 @@ public class RestClient {
     }
 
     public <T, R> T getRequest(String url, HttpHeaders headers, R request, Class<T> responseType) {
+        GlobalLogger.info("request : ",url,headers,request);
+
         try {
             HttpEntity<R> entity = new HttpEntity<>(request, headers);
             T response = restTemplate.exchange(url, HttpMethod.GET, entity, responseType).getBody();
@@ -95,6 +106,8 @@ public class RestClient {
     }
 
     public <T> T getRequest(String url, HttpHeaders headers, String urlParams, Class<T> responseType) {
+        GlobalLogger.info("request : ",url,headers,urlParams);
+
         try {
             headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
             HttpEntity<String> entity = new HttpEntity<>(urlParams, headers);
@@ -107,12 +120,14 @@ public class RestClient {
     }
 
     private <T> T validate(T response){
+        GlobalLogger.info("response:",response);
         validateIsNull(response);
 
         return response;
     }
 
     private <T> void validateIsNull(T response){
+
         if (response == null) {
             throw new RestClientException(RestClientErrorCode.RESPONSE_IS_NULL);
         }
