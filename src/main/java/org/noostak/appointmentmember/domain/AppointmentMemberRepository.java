1d7d8b83cb92f095ebba5d01dfb1f2f3afa35707
0tae <choiyt3465@naver.com>
2025-03-14 01:14:05 +0900

    refactor(AppointmentMemberRepository) : 예외처리를 Repository 내부에서 수행하도록 수정

--- src/main/java/org/noostak/appointmentmember/domain/AppointmentMemberRepository.java
+++ src/main/java/org/noostak/appointmentmember/domain/AppointmentMemberRepository.java
@@ -1,9 +1,17 @@
 package org.noostak.appointmentmember.domain;
 
+import org.noostak.appointment.common.exception.AppointmentErrorCode;
+import org.noostak.appointmentmember.common.exception.AppointmentMemberException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AppointmentMemberRepository extends JpaRepository<AppointmentMember, Long>, AppointmentMemberRepositoryCustom {
     List<AppointmentMember> findByAppointmentId(Long appointmentId);
+
+    default AppointmentMember getByMemberIdAndAppointmentId(Long memberId, Long appointmentId){
+        return findByMemberIdAndAppointmentId(memberId,appointmentId).
+                orElseThrow(()->new AppointmentMemberException(AppointmentErrorCode.APPOINTMENT_NOT_FOUND));
+    }
 }
