e51dd54e37a1e0489c8d063465530e385200961c
0tae <choiyt3465@naver.com>
2025-03-14 01:13:59 +0900

    refactor(AppointmentOptionRepository) : 예외처리를 Repository 내부에서 수행하도록 수정

--- src/main/java/org/noostak/appointmentoption/domain/AppointmentOptionRepository.java
+++ src/main/java/org/noostak/appointmentoption/domain/AppointmentOptionRepository.java
@@ -1,8 +1,15 @@
 package org.noostak.appointmentoption.domain;
 
+import org.noostak.likes.common.exception.LikesErrorCode;
+import org.noostak.likes.common.exception.LikesException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AppointmentOptionRepository extends JpaRepository<AppointmentOption, Long>, AppointmentOptionRepositoryCustom {
+
+    default AppointmentOption getByAppointmentOptionId(Long appointmentId){
+        return findById(appointmentId)
+                .orElseThrow(() -> new LikesException(LikesErrorCode.OPTION_NOT_FOUND));
+    }
 }
