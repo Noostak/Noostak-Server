c92747924ef57f68187cdd5908771657c3d28b4a
0tae <choiyt3465@naver.com>
2025-03-14 16:54:29 +0900

    fix(KakaoUserInfoResponse) : NoArgsConstructor 어노테이션 추가

--- src/main/java/org/noostak/auth/dto/kakao/KakaoUserInfoResponse.java
+++ src/main/java/org/noostak/auth/dto/kakao/KakaoUserInfoResponse.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.noostak.auth.common.exception.KakaoApiErrorCode;
 import org.noostak.auth.common.exception.KakaoApiException;
@@ -12,6 +13,7 @@ import org.noostak.auth.common.exception.KakaoApiException;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @ToString
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class KakaoUserInfoResponse {
@@ -35,6 +37,7 @@ public class KakaoUserInfoResponse {
 
     @Getter
     @AllArgsConstructor
+    @NoArgsConstructor
     @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
     public static class Properties {
         private String nickname;
@@ -43,6 +46,7 @@ public class KakaoUserInfoResponse {
 
     @Getter
     @AllArgsConstructor
+    @NoArgsConstructor
     @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
     public static class KakaoAccount {
         private boolean profileNicknameNeedsAgreement;
@@ -51,6 +55,7 @@ public class KakaoUserInfoResponse {
 
         @Getter
         @AllArgsConstructor
+        @NoArgsConstructor
         @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
         public static class Profile {
             private String nickname;
