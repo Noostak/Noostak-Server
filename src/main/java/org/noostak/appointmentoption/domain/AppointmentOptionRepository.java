2895f49537da0dc57cd9c878e96d59a277d34ae7
0tae <choiyt3465@naver.com>
2025-03-15 18:19:10 +0900

    feat(AppointmentOptionRepository) : 쿼리 메소드 추가 - 약속과 시작, 끝 시간이 일치하는 AppointmentOption을 찾는 메소드 추가

--- src/main/java/org/noostak/appointmentoption/domain/AppointmentOptionRepository.java
+++ src/main/java/org/noostak/appointmentoption/domain/AppointmentOptionRepository.java
@@ -1,9 +1,13 @@
 package org.noostak.appointmentoption.domain;
 
+import org.noostak.appointment.domain.Appointment;
 import org.noostak.likes.common.exception.LikesErrorCode;
 import org.noostak.likes.common.exception.LikesException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Repository
 public interface AppointmentOptionRepository extends JpaRepository<AppointmentOption, Long>, AppointmentOptionRepositoryCustom {
@@ -11,5 +15,13 @@ public interface AppointmentOptionRepository extends JpaRepository<AppointmentOp
     default AppointmentOption getByAppointmentOptionId(Long appointmentId){
         return findById(appointmentId)
                 .orElseThrow(() -> new LikesException(LikesErrorCode.OPTION_NOT_FOUND));
+    }
+
+    Optional<AppointmentOption> findFirstByAppointmentAndStartTimeAndEndTime
+            (Appointment appointment, LocalDateTime startTime, LocalDateTime endTime);
+
+    default AppointmentOption getByAppointmentAndTimes(Appointment appointment, AppointmentOption option){
+        return findFirstByAppointmentAndStartTimeAndEndTime(appointment,option.getStartTime(),option.getEndTime())
+                .orElse(null);
     }
 }
