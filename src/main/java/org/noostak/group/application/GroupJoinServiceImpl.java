6dc3cd7aa0dde9c20199f8492164f53a815f6ecc
0tae <choiyt3465@naver.com>
2025-03-18 20:00:27 +0900

    refactor(GroupJoinServiceImpl) : 그룹 중복 참여 여부 유효성 검사 구현

--- src/main/java/org/noostak/group/application/GroupJoinServiceImpl.java
+++ src/main/java/org/noostak/group/application/GroupJoinServiceImpl.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.noostak.appointment.domain.Appointment;
 import org.noostak.appointment.domain.AppointmentRepository;
 import org.noostak.appointmentmember.application.AppointmentMemberSaveService;
+import org.noostak.group.common.exception.GroupErrorCode;
+import org.noostak.group.common.exception.GroupException;
 import org.noostak.group.domain.Group;
 import org.noostak.group.domain.GroupRepository;
 import org.noostak.group.dto.response.GroupJoinResponse;
@@ -31,8 +33,9 @@ public class GroupJoinServiceImpl implements GroupJoinService {
         Member member = memberRepository.getById(memberId);
         Group group = groupRepository.findByCode(inviteCode);
 
-        MemberGroup memberGroup = MemberGroup.of(member, group);
+        validate(group,member);
 
+        MemberGroup memberGroup = MemberGroup.of(member, group);
         memberGroupRepository.save(memberGroup);
 
         // 그룹 내 진행 중인 약속에 대해, 멤버-약속 관계 형성
@@ -47,5 +50,12 @@ public class GroupJoinServiceImpl implements GroupJoinService {
                 .stream().filter(Appointment::inProgress)
                 .forEach(appointment ->
                         appointmentMemberSaveService.saveAppointmentMember(appointment, member));
+    }
+
+
+    private void validate(Group group, Member member){
+        if(memberGroupRepository.existsByGroupAndMember(group,member)){
+            throw new GroupException(GroupErrorCode.GROUP_ALREADY_EXISTS);
+        }
     }
 }
