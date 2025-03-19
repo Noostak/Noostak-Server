cc7b52d058197bbbdcbe21c05e2bc9e701d39b99
0tae <choiyt3465@naver.com>
2025-03-19 16:23:01 +0900

    refactor(GlobalExceptionHandler) : 코드 리팩토링 및 MISSING_HEADER에 대한 예외처리 추가

--- src/main/java/org/noostak/global/error/handler/GlobalExceptionHandler.java
+++ src/main/java/org/noostak/global/error/handler/GlobalExceptionHandler.java
@@ -1,60 +1,63 @@
 package org.noostak.global.error.handler;
 
 import org.noostak.global.error.core.BaseException;
+import org.noostak.global.error.core.ErrorCode;
 import org.noostak.global.error.core.ErrorResponse;
 import org.noostak.global.utils.GlobalLogger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(NoResourceFoundException.class)
-    public ResponseEntity<ErrorResponse>
-    handleNoResourceException(NoResourceFoundException e) {
-        GlobalLogger.error(e.toString());
 
-        return ResponseEntity
-                .status(GlobalErrorCode.RESOURCE_NOT_FOUND.getStatus())
-                .body(ErrorResponse.of(GlobalErrorCode.RESOURCE_NOT_FOUND));
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<ErrorResponse> handleMissingRequestHeaderException(MissingRequestHeaderException e){
+        return getErrorResponse(e,GlobalErrorCode.MISSING_HEADER);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoResourceException(NoResourceFoundException e) {
+        return getErrorResponse(e,GlobalErrorCode.RESOURCE_NOT_FOUND);
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<ErrorResponse>
         handleMethodNotAllowedException(HttpRequestMethodNotSupportedException e) {
-        GlobalLogger.error(e.toString());
-
-        return ResponseEntity
-                .status(GlobalErrorCode.METHOD_NOT_ALLOWED.getStatus())
-                .body(ErrorResponse.of(GlobalErrorCode.METHOD_NOT_ALLOWED));
+        return getErrorResponse(e,GlobalErrorCode.METHOD_NOT_ALLOWED);
     }
 
     @ExceptionHandler(BaseException.class)
     public ResponseEntity<ErrorResponse> handleBaseException(BaseException e) {
-        GlobalLogger.error(e.toString());
-
-        return ResponseEntity
-                .status(e.getErrorCode().getStatus())
-                .body(ErrorResponse.of(e.getErrorCode(),e.getMessage()));
+        return getErrorResponse(e,e.getErrorCode());
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
-        GlobalLogger.error(e.toString());
-
-        return ResponseEntity
-                .status(GlobalErrorCode.INTERNAL_SERVER_ERROR.getStatus())
-                .body(ErrorResponse.of(GlobalErrorCode.INTERNAL_SERVER_ERROR));
+        return getErrorResponse(e,GlobalErrorCode.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        return getErrorResponse(e,GlobalErrorCode.INTERNAL_SERVER_ERROR);
+    }
+
+    private static ResponseEntity<ErrorResponse> getErrorResponse(Exception e, GlobalErrorCode errorCode) {
         GlobalLogger.error(e.toString());
 
         return ResponseEntity
-                .status(GlobalErrorCode.INTERNAL_SERVER_ERROR.getStatus())
-                .body(ErrorResponse.of(GlobalErrorCode.INTERNAL_SERVER_ERROR));
+                .status(errorCode.getStatus())
+                .body(ErrorResponse.of(errorCode));
+    }
+
+    private static ResponseEntity<ErrorResponse> getErrorResponse(Exception e, ErrorCode errorCode) {
+        GlobalLogger.error(e.toString());
+
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(ErrorResponse.of(errorCode, e.getMessage()));
     }
 }
