899c03186713d33645a914bf03ee4530128d6676
0tae <choiyt3465@naver.com>
2025-03-19 16:22:02 +0900

    feat(GlobalErrorCode) : MISSING_HEADER에 대한 에러 정의

--- src/main/java/org/noostak/global/error/handler/GlobalErrorCode.java
+++ src/main/java/org/noostak/global/error/handler/GlobalErrorCode.java
@@ -13,6 +13,7 @@ public enum GlobalErrorCode implements ErrorCode {
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 메소드입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "권한이 없습니다."),
+    MISSING_HEADER(HttpStatus.BAD_REQUEST, "요청에 필요한 헤더가 존재하지 않습니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN, "접근이 금지되었습니다.");
 
     public static final String PREFIX = "[GLOBAL ERROR] ";
