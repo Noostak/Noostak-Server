d47f428e88a1c438cbadd69902c187641ded0f37
0tae <choiyt3465@naver.com>
2025-03-19 18:51:20 +0900

    refactor(ApiPathFilter) : 허용 api 경로 추가 및 코드 리팩토링

--- src/main/java/org/noostak/global/config/ApiPathFilter.java
+++ src/main/java/org/noostak/global/config/ApiPathFilter.java
@@ -12,21 +12,27 @@ import java.io.IOException;
 public class ApiPathFilter extends OncePerRequestFilter {
 
     private static final String API_PATTERN = "/api/v1";
+    private static final String HEALTH_PATTERN = "/actuator/health";
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         String requestUri = request.getRequestURI();
+
         GlobalLogger.info("[Filter] 요청 호스트 정보:",request.getRemoteHost(),request.getRemotePort());
         GlobalLogger.info("[Filter] 요청 경로 정보:",request.getMethod(),request.getRequestURI());
 
-        if (!isApiRequest(requestUri)) {
-            response.sendError(HttpServletResponse.SC_FORBIDDEN);
-        } else {
+        if (isApiRequest(requestUri) || isHealthCheck(requestUri)) {
             filterChain.doFilter(request, response);
         }
+
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
     }
 
+    private boolean isHealthCheck(String requestUri){
+        return requestUri.startsWith(HEALTH_PATTERN);
+    }
     private boolean isApiRequest(String requestUri) {
         return requestUri.startsWith(API_PATTERN);
     }
