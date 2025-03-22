3826328c4d2225137a538098d05f2f76730c257a
0tae <choiyt3465@naver.com>
2025-03-23 03:51:03 +0900

    feat(GroupService) : 그룹을 생성하면 그룹에 자동으로 가입되도록 수정

--- src/main/java/org/noostak/group/application/GroupService.java
+++ src/main/java/org/noostak/group/application/GroupService.java
@@ -31,6 +31,10 @@ public class GroupService {
 
     public GroupCreateResponse createGroup(Long memberId, GroupCreateRequest request) throws IOException {
         GroupCreateInternalResponse response = groupCreateService.createGroup(memberId, request);
+
+        String inviteCode = response.group().getCode().value();
+        groupJoinService.join(memberId, inviteCode);
+
         return GroupCreateResponse.of(response.group(), response.groupProfileImageUrl());
     }
 
