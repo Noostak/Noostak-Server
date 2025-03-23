44aae7d82c8b56b3369a1391165eea80cdd39f16
권장순 <jsoonworld@gmail.com>
2025-03-24 07:59:27 +0900

    feat(AppointmentOptionController) : 확정된 약속 옵션 조회 API 구현
    - /{appointmentOptionId}/confirmed 경로로 확정된 약속 옵션을 조회할 수 있는 GET API 추가
    - memberId는 @RequestAttribute를 통해 추출
    - 서비스 계층에서 AppointmentConfirmedOptionResponse 반환
    - 성공 응답은 CONFIRMED_APPOINTMENT_RETRIEVED 코드와 함께 반환

--- src/main/java/org/noostak/appointmentoption/api/AppointmentOptionController.java
+++ src/main/java/org/noostak/appointmentoption/api/AppointmentOptionController.java
@@ -2,11 +2,13 @@ package org.noostak.appointmentoption.api;
 
 import lombok.RequiredArgsConstructor;
 import org.noostak.appointmentoption.application.AppointmentOptionService;
+import org.noostak.appointmentoption.dto.response.confirmed.AppointmentConfirmedOptionResponse;
 import org.noostak.global.success.SuccessResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static org.noostak.appointment.common.success.AppointmentSuccessCode.APPOINTMENT_CONFIRMED;
+import static org.noostak.appointment.common.success.AppointmentSuccessCode.CONFIRMED_APPOINTMENT_RETRIEVED;
 
 @RestController
 @RequestMapping("/api/v1/appointment-options")
@@ -21,5 +23,14 @@ public class AppointmentOptionController {
     ) {
         appointmentService.confirmAppointment(appointmentOptionId);
         return ResponseEntity.ok(SuccessResponse.of(APPOINTMENT_CONFIRMED));
+    }
+
+    @GetMapping("/{appointmentOptionId}/confirmed")
+    public ResponseEntity<SuccessResponse> getConfirmedAppointment(
+            @RequestAttribute Long memberId,
+            @PathVariable Long appointmentOptionId
+    ) {
+        AppointmentConfirmedOptionResponse appointmentConfirmedOptionResponse = appointmentService.getConfirmedAppointmentOption(memberId, appointmentOptionId);
+        return ResponseEntity.ok(SuccessResponse.of(CONFIRMED_APPOINTMENT_RETRIEVED, appointmentConfirmedOptionResponse));
     }
 }
