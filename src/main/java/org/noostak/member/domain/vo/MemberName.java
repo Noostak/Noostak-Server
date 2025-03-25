94a8b5b730ec5550482b712960ac494802db3765
0tae <choiyt3465@naver.com>
2025-03-25 23:13:53 +0900

    refactor(MemberName) : 초성을 허용할 수 있도록 수정

--- src/main/java/org/noostak/member/domain/vo/MemberName.java
+++ src/main/java/org/noostak/member/domain/vo/MemberName.java
@@ -11,7 +11,7 @@ import java.util.regex.Pattern;
 @EqualsAndHashCode
 public class MemberName {
     private static final int MAX_LENGTH = 10;
-    private static final Pattern INVALID_PATTERN = Pattern.compile("[^\uAC00-\uD7A3a-zA-Z\uD83C-\uDBFF\uDC00-\uDFFF\u200D\uFE0F]");
+    private static final Pattern INVALID_PATTERN = Pattern.compile("[^ㄱ-힣a-zA-Z\uD83C-\uDBFF\uDC00-\uDFFF\u200D\uFE0F]");
 
     private final String name;
 
