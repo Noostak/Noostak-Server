94970dc22b0c8a923a7b690190ff03c76009ee3d
권장순 <jsoonworld@gmail.com>
2025-04-03 15:10:42 +0900

    refactor(AppointmentName) : 약속 이름 검증에 맞게 테스트 개선

--- src/test/java/org/noostak/appointment/domain/vo/AppointmentNameTest.java
+++ src/test/java/org/noostak/appointment/domain/vo/AppointmentNameTest.java
@@ -1,11 +1,9 @@
 package org.noostak.appointment.domain.vo;
 
-import org.assertj.core.util.Strings;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.noostak.appointment.common.exception.AppointmentErrorCode;
 import org.noostak.appointment.common.exception.AppointmentException;
@@ -27,13 +25,26 @@ class AppointmentNameTest {
                 "Lunch Meeting",
                 "개발자 모임",
                 "스터디 그룹",
-                "프로젝트 회의"
+                "프로젝트 회의",
+                "미팅@123",
+                "테스트#이름",
+                "개발모임*스터디",
+                "새로운~회의",
+                "중요 회의!",
         })
         void shouldCreateAppointmentNameSuccessfully(String validName) {
-            // Given & When
             AppointmentName appointmentName = AppointmentName.from(validName);
+            assertThat(appointmentName.value()).isEqualTo(validName);
+        }
 
-            // Then
+        @ParameterizedTest
+        @DisplayName("약속 이름이 20자 이하일 경우 성공")
+        @ValueSource(strings = {
+                "12345678901234567890", // 20자
+                "약속 이름입니다."       // 한글 포함된 예시
+        })
+        void shouldAllowNameWithUpTo20Characters(String validName) {
+            AppointmentName appointmentName = AppointmentName.from(validName);
             assertThat(appointmentName.value()).isEqualTo(validName);
         }
     }
@@ -43,48 +54,14 @@ class AppointmentNameTest {
     class FailureCases {
 
         @ParameterizedTest
-        @DisplayName("약속 이름이 null이거나 비어 있는 경우")
-        @NullAndEmptySource
-        void shouldThrowExceptionForNullOrEmptyName(String invalidName) {
-            assertThatThrownBy(() -> AppointmentName.from(invalidName))
-                    .isInstanceOf(AppointmentException.class)
-                    .hasMessageContaining(AppointmentErrorCode.APPOINTMENT_NAME_NOT_EMPTY.getMessage());
-        }
-
-        @ParameterizedTest
-        @DisplayName("약속 이름이 공백만 포함된 경우")
-        @ValueSource(strings = {" ", "  ", "\t", "\n"})
-        void shouldThrowExceptionForOnlyWhitespaceName(String invalidName) {
-            assertThatThrownBy(() -> AppointmentName.from(invalidName))
-                    .isInstanceOf(AppointmentException.class)
-                    .hasMessageContaining(AppointmentErrorCode.APPOINTMENT_NAME_NOT_EMPTY.getMessage());
-        }
-
-        @ParameterizedTest
-        @DisplayName("약속 이름의 길이가 50자를 초과하는 경우")
-        @CsvSource({
-                "abcdefghijklmnopqrstuvwxyz12345678901234567890aaaaaaaabbbbbbbb",
-                "회의이름이너무길어서이것은유효하지않습니다이것은50자초과입니다정말로길다길어이거는너무길다rrrrrrrrrr"
+        @DisplayName("약속 이름이 20자를 초과하는 경우")
+        @ValueSource(strings = {
+                "123456789012345678901", // 21자
         })
         void shouldThrowExceptionForNameExceedingMaxLength(String invalidName) {
             assertThatThrownBy(() -> AppointmentName.from(invalidName))
                     .isInstanceOf(AppointmentException.class)
                     .hasMessageContaining(AppointmentErrorCode.INVALID_APPOINTMENT_NAME_LENGTH.getMessage());
-        }
-
-        @ParameterizedTest
-        @DisplayName("약속 이름에 허용되지 않은 특수 문자가 포함된 경우")
-        @CsvSource({
-                "미팅@123",
-                "테스트#이름",
-                "개발모임*스터디",
-                "새로운~회의",
-                "중요 회의!"
-        })
-        void shouldThrowExceptionForNameWithSpecialCharacters(String invalidName) {
-            assertThatThrownBy(() -> AppointmentName.from(invalidName))
-                    .isInstanceOf(AppointmentException.class)
-                    .hasMessageContaining(AppointmentErrorCode.INVALID_APPOINTMENT_NAME_CHARACTER.getMessage());
         }
     }
 }
